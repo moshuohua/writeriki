e067469f59f2bd32a8205ff8ee95186b6ceaaed0
buxiangshuo <moshuohua@gmail.com>
2013-04-23 19:52:02 +0800

    add joining projects list in "my projects"

--- app/controllers/myprojects_controller.rb
+++ app/controllers/myprojects_controller.rb
@@ -2,12 +2,14 @@ class MyprojectsController < ApplicationController
 
   def index
     @user = current_user
-    @joinings = Project.where("user_id = ?", Duty.find_all_by_user_id(@user.id))
+    @joinings = Duty.all
+    @pros = Project.all
     @projects = Project.where("user_id = ?", @user.id)
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @projects }
+      format.json { render json: @joinings }
     end
   end
 
